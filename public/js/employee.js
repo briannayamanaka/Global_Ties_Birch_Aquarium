b7e536c8111aab7a89c5ce071e304615667a3951
WINDOWS-TJ8EIAL\leona <leonanh95@gmail.com>
2017-05-23 22:44:00 -0700

    Revert "writing user authentication. So far it's not working. Plus I changed employee ID to email (since firebase only does email authentication). also, we should not be storing user passwords in our database as plain text (should be encrypted) so I'm taking that out of our database and moving it over to Firebase auth"
    This reverts commit 4d62c07826bfebf24529c1b9729053ffdeb368b6.

--- public/js/employee.js
+++ public/js/employee.js
@@ -14,38 +14,30 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 /* Write an object to the Firebase database under the database Employees tree
-* with the key idInput and the values idInput and pwdInput.
+* with the key idInput and the values idInput and pwdInput. 
 * Example: writeToFirebase(42, 'pwd123')
 *   will store under key 42 an object containing id 42, paswword pwd123
 *   This object is then stored at https://birchaquarium-fd036.firebaseio.com/42 */
-function writeToFirebase(emailInput, pwdInput) {
-
+function writeToFirebase(idInput, pwdInput) {
+    
     // Write the object under the database root "users"
     // Datbase schema: RFID tree-->user object that contains links id with name and age group
-    database.ref("Employees/" + emailInput).set({
-        email: emailInput,
+    database.ref("Employees/" + idInput).set({
+        id: idInput,
         password: pwdInput
     });
 }
 
-
 // This function will get called when the button is clicked
 function login() {
-
+    
     //Get values from the text boxes
-    var emailText = document.getElementById("emailInput").value;
+    var rfidText = document.getElementById("idInput").value;
     var nameText = document.getElementById("pwdInput").value;
 
     // debugging (press Ctrl+shift+I) for console
     console.log("update?")
-
+    
     // Write user object to Firebase under the key rfidText
-    writeToFirebase(emailText, nameText);
-
-    // User should be created within Firebase "authentication" tab
-    firebase.auth().createUserWithEmailAndPassword(emailText, pwdText).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
+    writeToFirebase(idText, pwdInput);
 }
