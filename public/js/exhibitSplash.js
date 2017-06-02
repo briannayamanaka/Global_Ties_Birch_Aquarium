f248077ed2e7ed81998b5b37537e617e3e8ace4d
Proud Heng <pheng@ucsd.edu>
2017-06-01 19:22:58 -0700

    Hopefully exhibit terminal RFID scan works
    Now only sends RFID from exhibit splash screen to exhibit terminal,
    then looks up user info in exhibit terminal

--- public/js/exhibitSplash.js
+++ public/js/exhibitSplash.js
@@ -5,16 +5,6 @@
 
 $(document).ready(function () {
 
-    /********** application configuration **********/
-    var config = {
-        apiKey: "AIzaSyAJi4sI7b5M9IkhEfLvn9OJ7TbRkNv-RE8",
-        authDomain: "birchaquarium-fd036.firebaseapp.com",
-        databaseURL: "https://birchaquarium-fd036.firebaseio.com/",
-        storageBucket: "birchaquarium-fd036.appspot.com"
-    };
-    //initialize application
-    firebase.initializeApp(config);
-
     "use strict";
     var scanned = false,
         scannedInput = [];
@@ -42,27 +32,8 @@ $(document).ready(function () {
 
                 if (scanned) {
                     console.log("scanned");
-                    // Read from Firebase database
-                    firebase.database().ref('/RFID/' + userID).once('value').then(function (snapshot) {
-                        // To get a specific field,
-                        // i.e. name, use `snapshot.val().name`
-                        var name = snapshot.val().name;
-                        var avatar = snapshot.val().avatar;
-                        var age = snapshot.val().ageGroup;
-                        var language = snapshot.val().language;
-
-                        console.log("Name: " + name);
-                        console.log("Avatar: " + avatar);
-                        console.log("Age: " + age);
-                        console.log("Language: " + language);
-
-                        //stores user info aka RFID locally in web browser for persistent data
-                        window.localStorage.setItem('name', name);
-                        window.localStorage.setItem('avatar', avatar);
-                        window.localStorage.setItem('age', age);
-                        window.localStorage.setItem('language', language);
-
-                    });
+                    //save RFID input
+                    window.localStorage.setItem('userID', userID);
 
                     //jump to exhibit_terminal page
                     window.location.href = "../html/exhibit_terminal.html";
--- public/js/exhibit_terminal.js
+++ public/js/exhibit_terminal.js
@@ -1,11 +1,28 @@
 // Authors: Leon
+/********** application configuration **********/
+var config = {
+    apiKey: "AIzaSyAJi4sI7b5M9IkhEfLvn9OJ7TbRkNv-RE8",
+    authDomain: "birchaquarium-fd036.firebaseapp.com",
+    databaseURL: "https://birchaquarium-fd036.firebaseio.com/",
+    storageBucket: "birchaquarium-fd036.appspot.com"
+};
+//initialize application
+firebase.initializeApp(config);
 
+var userID = window.localStorage.getItem('userID');
+console.log("RFID: " + userID);
 
-var name = window.localStorage.getItem('name');
-var avatar = window.localStorage.getItem('avatar');
-var age = window.localStorage.getItem('age');
-var language = window.localStorage.getItem('language');
-console.log("Name: " + name);
-console.log("Avatar: " + avatar);
-console.log("Age: " + age);
-console.log("Language: " + language);
+// Read from Firebase database
+firebase.database().ref('/RFID/' + userID).once('value').then(function (snapshot) {
+    // To get a specific field,
+    // i.e. name, use `snapshot.val().name`
+    var name = snapshot.val().name;
+    var avatar = snapshot.val().avatar;
+    var age = snapshot.val().ageGroup;
+    var language = snapshot.val().language;
+
+    console.log("Name: " + name);
+    console.log("Avatar: " + avatar);
+    console.log("Age: " + age);
+    console.log("Language: " + language);
+});
